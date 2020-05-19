5bc207e391d7955752519e091794a3f06c05cca4
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-05-19 09:48:16 +0100

    update script to inckude click module & provide more instance information

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,25 @@
 import boto3
+import click
+
+s3 = boto3.resource('s3')
+ec2 = boto3.resource('ec2')
+
+@click.command()
+def list_instances():
+  "List EC2 instances"
+  for i in ec2.instances.all():
+    print(', '.join((
+      i.id,
+      i.instance_type,
+      i.placement['AvailabilityZone'],
+      i.state['Name'],
+      i.public_dns_name)))
+  return
+
+def list_buckets():
+  "List S3 buckets"
+  for bucket in s3.buckets.all():
+    print(bucket.name)
 
 if __name__ == '__main__':
-  s3 = boto3.resource('s3')
-  for bucket in s3.buckets.all():
-      print(bucket.name)
-  ec2 = boto3.resource('ec2')
-  for i in ec2.instances.all():
-      print(i)
+  list_instances()
